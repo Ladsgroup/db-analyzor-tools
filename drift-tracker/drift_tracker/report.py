d06a2038f88ba73008cd5b3c71b3f14258fbf88e
Amir Sarabadani <Ladsgroup@gmail.com>
2021-05-16 07:44:51 +0200

    Fix the titles and get the new reports

--- drift-tracker/drift_tracker/report.py
+++ drift-tracker/drift_tracker/report.py
@@ -5,12 +5,12 @@ import requests
 from .tracking import get_tracking_internal
 
 titles = {
-    'field-type-mismatch': 'Mismatching field type of {1}.{0}',
-    'field-nullable-mismatch': 'Mismatching field nullability of {1}.{0}',
-    'field-unsigned-mismatch': 'Mismatching field unsigned status of {1}.{0}',
-    'field-mismatch-codebase-extra': 'Extra field {1}.{0} in code',
-    'field-mismatch-prod-extra': 'Extra field {1}.{0} in production',
-    'field-size-mismatch': 'Mismatching field size of {1}.{0}',
+    'field-type-mismatch': 'Mismatching field type of {0}.{1}',
+    'field-nullable-mismatch': 'Mismatching field nullability of {0}.{1}',
+    'field-unsigned-mismatch': 'Mismatching field unsigned status of {0}.{1}',
+    'field-mismatch-codebase-extra': 'Extra field {0}.{1} in code',
+    'field-mismatch-prod-extra': 'Extra field {0}.{1} in production',
+    'field-size-mismatch': 'Mismatching field size of {0}.{1}',
     'index-mismatch-prod-extra': 'Extra index {1} in production on table {0}',
     'index-mismatch-code-extra': 'Extra index {1} in codebase on table {0}',
 }
@@ -18,7 +18,7 @@ titles = {
 
 def get_report(category, untracked_only=False):
     data = requests.get(
-        'https://people.wikimedia.org/~ladsgroup/by_drift_type_drifts.json').json()
+        'https://people.wikimedia.org/~ladsgroup/by_drift_type_drifts_core.json').json()
     tracked = get_tracking_internal()
     data = OrderedDict(
         sorted(data.items(), key=lambda t: len(t[1]), reverse=True))
